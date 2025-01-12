dfb617ba0f47163befa02bac60bc24a0cb1e3eea
Drakatoa <ragoel123@gmail.com>
2025-01-12 17:55:48 -0600

    hold to shoot and look around while moving

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -11,12 +11,14 @@ const ZOOM_SMOOTHNESS = 5.0  # Higher values make zooming faster
 
 # Shooting constants
 @export var ability_scene = preload("res://Ability.gd")  # Load the Ability script
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animated_sprite = $Protagonist
 
 # Variables
 var target_zoom = Vector2(1, 1)  # The zoom level we are transitioning to
 var ability  # Instance of the ability class
 var dir = "down"
+var is_shooting = false
+var shoot_timer = 0.0
 
 func _ready():
 	
@@ -31,46 +33,34 @@ func _physics_process(delta):
 	#print("Velocity:", self.velocity)
 	#print("Length:", self.velocity.length())
 	#print("Current animation:", $AnimatedSprite2D.animation)
-
+	# Calculate the angle between the character and the mouse
+	var mouse_position = get_global_mouse_position()
+	var angle_to_mouse = (mouse_position - global_position).angle()
+	# Determine direction based on the angle
 	if self.velocity.length() < 0.1:  # If the character is not moving
 		if dir == "right":
-			$AnimatedSprite2D.animation = "right_idle"
+			$Protagonist.animation = "right_idle"
 		elif dir == "left":
-			$AnimatedSprite2D.animation = "left_idle"
+			$Protagonist.animation = "left_idle"
 		elif dir == "up":
-			$AnimatedSprite2D.animation = "up_idle"
+			$Protagonist.animation = "up_idle"
 		else:
-			$AnimatedSprite2D.animation = "idle"
-		$AnimatedSprite2D.play()
-		#print("Playing idle")
-	else:
-		if abs(self.velocity.x) > abs(self.velocity.y):
-			if self.velocity.x > 0:
-				if $AnimatedSprite2D.animation != "run_right":
-					dir = "right"
-					$AnimatedSprite2D.animation = "run_right"
-					$AnimatedSprite2D.play()
-			else:
-				if $AnimatedSprite2D.animation != "run_left":
-					dir = "left"
-					$AnimatedSprite2D.animation = "run_left"
-					$AnimatedSprite2D.play()
+			$Protagonist.animation = "idle"
+		$Protagonist.play()
+	else:  # If the character is moving
+		if angle_to_mouse > -PI / 4 and angle_to_mouse <= PI / 4:
+			play_animation("run_right")
+			dir = "right"
+		elif angle_to_mouse > PI / 4 and angle_to_mouse <= 3 * PI / 4:
+			play_animation("run_down")
+			dir = "down"
+		elif angle_to_mouse > -3 * PI / 4 and angle_to_mouse <= -PI / 4:
+			play_animation("run_up")
+			dir = "up"
 		else:
-			if self.velocity.y > 0:
-				if $AnimatedSprite2D.animation != "run_down":
-					dir = "down"
-					$AnimatedSprite2D.animation = "run_down"
-					$AnimatedSprite2D.play()
-			else:
-				if $AnimatedSprite2D.animation != "run_up":
-					dir = "up"
-					$AnimatedSprite2D.animation = "run_up"
-					$AnimatedSprite2D.play()
-	#else:
-		# Handle other animations, e.g., "run"
-		#if animated_sprite.animation != "run":
-			#animated_sprite.animation = "run"
-			#animated_sprite.play()
+			play_animation("run_left")
+			dir = "left"
+
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
 		self.velocity = direction * SPEED
@@ -81,6 +71,23 @@ func _physics_process(delta):
 
 	# Smoothly interpolate the zoom level
 	$Camera2D.zoom = $Camera2D.zoom.lerp(target_zoom, ZOOM_SMOOTHNESS * delta)
+	
+	# Shooting logic
+	if Input.is_action_pressed("shoot"):
+		if shoot_timer <= 0.0:  # Only shoot if timer allows
+			shoot_projectile()
+			shoot_timer = ability.cooldown_time  # Reset the cooldown timer
+	else:
+		is_shooting = false  # Reset shooting state when button is released
+
+	# Decrease the shoot timer
+	if shoot_timer > 0.0:
+		shoot_timer -= delta
+
+func play_animation(animation_name: String):
+	if $Protagonist.animation != animation_name:
+		$Protagonist.animation = animation_name
+		$Protagonist.play()
 
 func _input(event):
 	# Zooming with mouse wheel
@@ -97,13 +104,17 @@ func _input(event):
 			target_zoom.x = clamp(target_zoom.x, MIN_ZOOM, MAX_ZOOM)
 			target_zoom.y = clamp(target_zoom.y, MIN_ZOOM, MAX_ZOOM)
 
-	# Shooting projectiles
-	if Input.is_action_just_pressed("shoot"):
-		shoot_projectile()
+	## Start shooting when "shoot" is pressed
+	#if Input.is_action_pressed("shoot"):
+		#is_shooting = true
+	## Stop shooting when "shoot" is released
+	#if Input.is_action_released("shoot"):
+		#is_shooting = false
 
 func shoot_projectile():
 	# Spawn position is slightly in front of the player
-	var spawn_position = global_position + Vector2(0, -10)  # Adjust as needed
-	var mouse_position = get_global_mouse_position()
-	var direction = mouse_position - global_position  # Direction towards the mouse
-	ability.use_ability(self, spawn_position, direction)
+	if ability.can_use:
+		var spawn_position = global_position + Vector2(0, -10)  # Adjust as needed
+		var mouse_position = get_global_mouse_position()
+		var direction = mouse_position - global_position  # Direction towards the mouse
+		ability.use_ability(self, spawn_position, direction)
